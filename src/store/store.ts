e253b8cd2ed6c21248c167074122960260ae90ad
mpcrockett <mpcrockett+github@gmail.com>
2023-05-30 16:28:28 -0400

    persist store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,36 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { configureStore, ThunkAction, Action, combineReducers, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { 
+  persistReducer, 
+  FLUSH, 
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER 
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import userReducer from './userSlice'
 import cartReducer from './cartSlice'
 
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1
+};
+
+const combinedReducers = combineReducers({
+  user: userReducer, cart: cartReducer
+});
+
+const persistedReducer = persistReducer(persistConfig, combinedReducers);
+
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-    cart: cartReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+    },
+  })
 });
 
 export type AppDispatch = typeof store.dispatch;
