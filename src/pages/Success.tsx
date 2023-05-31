a654504c87ac4047d9218eb8b1ecc574639e986a
mpcrockett <mpcrockett+github@gmail.com>
2023-05-31 18:53:21 -0400

    error fixes, success page

--- src/pages/Success.tsx
+++ src/pages/Success.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom";
+import NavButton from "../components/NavButton";
+import BackgroundBox from "../components/BackgroundBox";
+import { Box, Heading } from "@chakra-ui/react";
 
 function Success() {
+  const navigate = useNavigate();
+
   return (
-    <div>Your order was successful.</div>
-  )
+    <BackgroundBox>
+      <Box
+        width="100%"
+        height="100%"
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+      >
+        <Box
+          bg="brand.cream.50"
+          width="650px"
+          height="450px"
+          alignSelf="center"
+          borderRadius='lg'
+          display='flex'
+          flexDirection='column'
+          alignContent='center'
+          justifyContent='center'
+        >
+          <Heading textAlign='center'>Your order was successful!</Heading>
+          <NavButton
+            label={"Click here to return to the homepage."}
+            clickHandler={() => navigate("/")}
+          />
+        </Box>
+      </Box>
+    </BackgroundBox>
+  );
 }
 
-export default Success
+export default Success;
